3a2026ba9da71a736de6cc7564e7226f9a2fb2ac
cmpezahe <cmpezahe@outlook.com>
2023-12-26 00:35:29 +0000

    'Additional feature'

--- backupfile/api.js
+++ backupfile/api.js
@@ -4,6 +4,7 @@ const api_url = "https://api.api-ninjas.com/v1/cars";
 
 // let carName = "Audi";
 
+// Assuming you have the following input elements in your HTML
 let searchName = document.getElementById("s_name");
 let searchYear = document.getElementById("s_date");
 let fuelType = document.getElementById("fuel_type");
@@ -23,7 +24,7 @@ fuelType.addEventListener("change", function () {
 const params = {
   make: "", // Initial value for make
   year: "", // Initial value for year
-  limit: 8,
+  limit: 6,
   fuel_type: "gas", // Initial value for fuel_type
   // You can adjust the limit as needed
 };
@@ -73,6 +74,7 @@ document.addEventListener("DOMContentLoaded", function () {
   loadInputValues();
   updateParamsAndFetchData(); // Call this to fetch data based on the loaded values
 });
+
 const options = {
   method: "GET",
   headers: {
@@ -115,25 +117,18 @@ async function updateCarContainer(apiCars) {
   carContainer.innerHTML = ""; // Clear existing content
 
   for (const car of apiCars) {
-    const imageUrl = await fetchPexelsImage(car.make);
+    const imageUrl = await fetchUnsplashImage(car.make);
     const carElement = createCarElement({ ...car, imageUrl });
     carContainer.appendChild(carElement);
   }
 }
 
 // Helper function to fetch an image from Unsplash based on make
-const pexelsApiKey =
-  "jamctTkMSQJkXNO604VYtsdn3gh9Byw90BaAhWJcD9USqg3zNMB7aWNy "; // Replace with your Pexels API key
-
-async function fetchRandomPexelsImages(make, count = 1) {
-  const pexelsUrl = `https://api.pexels.com/v1/search?query=${make}&per_page=${count}`;
+async function fetchUnsplashImage(make) {
+  const unsplashUrl = `https://api.unsplash.com/photos/random?query=${make}&client_id=${unsplashAccessKey}`;
 
   try {
-    const response = await fetch(pexelsUrl, {
-      headers: {
-        Authorization: pexelsApiKey,
-      },
-    });
+    const response = await fetch(unsplashUrl);
 
     if (!response.ok) {
       throw new Error(`Error: ${response.status} - ${response.statusText}`);
@@ -141,35 +136,16 @@ async function fetchRandomPexelsImages(make, count = 1) {
 
     const data = await response.json();
 
-    // Extract and return an array of photo URLs
-    return data.photos.map((photo) => photo.src.large);
+    // Return the regular image URL if available, otherwise return a default image
+    return data.urls?.regular || "https://via.placeholder.com/400";
   } catch (error) {
-    console.error("Error fetching images from Pexels:", error);
-    return [];
+    console.error("Error fetching image from Unsplash:", error);
+
+    // Return a custom image URL when there's an error or no image available
+    return "assets/img/7184627.jpg";
   }
 }
 
-// ...
-
-// Helper function to update the carContainer with new data
-async function updateCarContainer(apiCars) {
-  const carContainer = document.getElementById("carContainer");
-  carContainer.innerHTML = ""; // Clear existing content
-
-  for (const car of apiCars) {
-    // Fetch multiple random images for each car
-    const imageUrls = await fetchRandomPexelsImages(car.make, 3); // Adjust count as needed
-
-    // Use a different image URL for each card
-    const imageUrl =
-      imageUrls.length > 0
-        ? imageUrls[Math.floor(Math.random() * imageUrls.length)]
-        : "https://via.placeholder.com/400";
-
-    const carElement = createCarElement({ ...car, imageUrl });
-    carContainer.appendChild(carElement);
-  }
-}
 // Helper function to create car HTML structure
 function createCarElement(car) {
   const container = document.createElement("div");
